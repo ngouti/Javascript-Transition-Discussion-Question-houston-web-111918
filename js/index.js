bbd2114dfb84f60cbacd7adb14c90129219caa02
github username <github email address>
2019-01-09 11:05:07 -0600

    booya

--- js/index.js
+++ js/index.js
@@ -1,8 +1,38 @@
-$('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+// $('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
 
-function saveLincoln(interval) {
-  if(isNaN(interval)) {
-    interval = 10000
-  }
-  $('#foreground').fadeOut(interval);
+// function saveLincoln(interval) {
+//   if(isNaN(interval)) {
+//     interval = 10000
+//   }
+//   $('#foreground').fadeOut(interval);
+// }
+
+function test(){
+
+  let x = document.getElementById("interval").value
+  // console.log(x)
+  
+  // let y = document.getElementById("foreground")
+  var y = document.querySelector("#foreground");
+
+  var fade = setInterval(function () {
+    if(x === NaN){
+      x = 10000
+    }
+    if (!y.style.opacity) {
+        y.style.opacity = 1;
+    }
+  //   if (y.style.opacity = 0) {
+  //     clearInterval(fade);
+  // } 
+    if (y.style.opacity < 0.1) {
+        clearInterval(fade);
+    } else {
+        y.style.opacity -= 0.1;
+    }
+}, x);
+
+    
 }
+
+
